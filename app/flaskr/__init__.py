36f28d17c6bad1e4cb2f0712d99bfb7838d65dff
Edward Chamberlain <edward.chamberlain@me.com>
2021-04-21 22:32:18 +0100

    naming clarity in config

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-from config import Config
+from config import Flask_Config
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Flask_Config)
 
 
 from flaskr import routes
